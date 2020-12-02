867a232f86e8d7f9f33b8baeb5f0a620497f6156
rahul003prasad <rahul003prasad@gmail.com>
2020-12-02 19:12:45 +0530

    Updating UI

--- Android App/Source Code/app/src/main/java/com/rahulprasad/infinityhaul/Home.java
+++ Android App/Source Code/app/src/main/java/com/rahulprasad/infinityhaul/Home.java
@@ -3,6 +3,7 @@ package com.rahulprasad.infinityhaul;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
     }
 }
